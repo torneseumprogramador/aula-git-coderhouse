b85a43cac78bcbe0f21162c4a385e88e38279664
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-11-29 20:16:29 -0300

    agora trabalhando com API

--- init.py
+++ init.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Bem-vindo à minha página com Flask!'
+    return jsonify(mensagem="Bem-vindo à minha api com Flask!")
 
 if __name__ == '__main__':
     app.run(debug=True)
